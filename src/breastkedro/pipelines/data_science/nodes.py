5b4beb700c8d954a4fade9df003375f9cf61303a
Francisco Ocampo <fjocampog@uqvirtual.edu.co>
2022-10-09 13:26:57 -0500

    nodos y pipelines ciencia de datos

--- src/breastkedro/pipelines/data_science/nodes.py
+++ src/breastkedro/pipelines/data_science/nodes.py
@@ -2,3 +2,61 @@
 This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.18.3
 """
+import importlib
+import logging
+from typing import Any, Dict
+
+import numpy as np
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.pipeline import Pipeline
+
+from sklearn.naive_bayes import GaussianNB
+
+
+logger = logging.getLogger(__name__)
+
+
+def train_model(x_features: np.ndarray,
+                y_target: np.ndarray,
+                parameters: Dict[str, Any]):
+    """Trains the classification model.
+    Args:
+        x_features: Training data
+        y_target: target column name
+        parameters: model parameters
+    Returns:
+        Trained model.
+    """
+    x_train = x_features
+    y_train = y_target
+
+    logger.info("training model")
+
+    model = GaussianNB()
+
+    mlflow.set_experiment('BreastCancer')
+    mlflow.set_tag("mlflow.runName", model.__class__.__name__)
+
+    model.fit(x_train, y_train)
+    return model
+
+
+def test_transform(x_test: pd.DataFrame,
+                   train_transformer: Pipeline) -> np.ndarray:
+    """ predictions for dataframe"""
+    logger.info("transform X_test")
+    x_test_transformed = train_transformer.transform(x_test)
+    mlflow.set_experiment('BreastCancer')
+    mlflow.log_param(f"shape test_transformed", x_test_transformed.shape)
+    return x_test_transformed
+
+
+def predict(model,
+            data: np.ndarray) -> np.ndarray:
+    """ predictions for dataframe"""
+    logger.info("predicting ")
+
+    pred = model.predict(data)
+    return pred
--- src/breastkedro/pipelines/data_science/pipeline.py
+++ src/breastkedro/pipelines/data_science/pipeline.py
@@ -6,5 +6,45 @@ generated using Kedro 0.18.3
 from kedro.pipeline import Pipeline, node, pipeline
 
 
+
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
+
+from .nodes import predict, train_model, test_transform
+
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=train_model,
+                inputs=["x_train_model_input",
+                        "y_train_model_input",
+                        'parameters'],
+                outputs="model_trained",
+                name="train_model"
+            ),
+            node(
+                func=predict,
+                inputs=["model_trained",
+                        "x_train_model_input"],
+                outputs="predictions_train",
+                name="predict_train"
+            ),
+            node(
+                func=test_transform,
+                inputs=["x_test", "column_transformers_pipeline"],
+                outputs="x_test_transformed",
+                name="x_test_transform",
+
+            ),
+            node(
+                func=predict,
+                inputs=["model_trained",
+                        "x_test_transformed"],
+                outputs="predictions_test",
+                name="predict_test"
+            )
+        ]
+    )
+
+
