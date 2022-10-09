72b26d668cbe5665801559d5e789b9cc9c2727e6
Francisco Ocampo <fjocampog@uqvirtual.edu.co>
2022-10-09 13:30:49 -0500

    nodos y pipelines model evaluation

--- src/breastkedro/pipelines/model_evaluation/nodes.py
+++ src/breastkedro/pipelines/model_evaluation/nodes.py
@@ -2,3 +2,41 @@
 This is a boilerplate pipeline 'model_evaluation'
 generated using Kedro 0.18.3
 """
+logger = logging.getLogger(__name__)
+
+def evaluate_model(predictions: np.ndarray,
+                   test_labels: pd.Series,
+                   name: str):
+    """
+    Evaluate the model by calculating the accuracy score.
+    """
+    score = recall_score(test_labels, predictions)
+
+    logger.info(f"Model accuracy {name}= {score}")
+
+    mlflow.set_experiment('BreastCancer')
+    mlflow.log_metric(f"recall {name}", score)
+
+    # parse the score to a string with only 4 decimal places
+    return f"{score:.4f}"
+
+
+def model_evaluation_check(x_train: np.ndarray,
+                           x_test: np.ndarray,
+                           y_train: np.ndarray,
+                           y_test: pd.Series,
+                           model):
+
+    y_test = y_test['diagnosis'].to_numpy()
+    train_ds = Dataset(x_train, label=y_train, cat_features=[])
+    test_ds = Dataset(x_test, label=y_test, cat_features=[])
+
+    evaluation_suite = model_evaluation()
+    suite_result = evaluation_suite.run(train_ds, test_ds, model)
+    mlflow.set_experiment('diagnosis')
+    mlflow.log_param(f"model evaluation validation", str(suite_result.passed()))
+    if not suite_result.passed():
+        #save report in data/08_reporting
+        suite_result.save_as_html('data/08_reporting/model_eval_check.html')
+        logger.error("model not pass evaluation tests")
+        print("model not pass evaluation tests")
--- src/breastkedro/pipelines/model_evaluation/pipeline.py
+++ src/breastkedro/pipelines/model_evaluation/pipeline.py
@@ -7,4 +7,33 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=evaluate_model,
+                inputs=["predictions_train",
+                        "y_train_model_input",
+                        'params:train'],
+                outputs="score_train",
+                name="train_model_evaluation"
+            ),
+            node(
+                func=evaluate_model,
+                inputs=["predictions_test",
+                        "y_test",
+                        'params:test'],
+                outputs="score_test",
+                name="test_model_evaluation"
+            ),
+            node(
+                func=model_evaluation_check,
+                inputs=["x_train_model_input",
+                        "x_test_transformed",
+                        "y_train_model_input",
+                        "y_test",
+                        "model_trained"],
+                outputs=None,
+                name="model_evaluation_check"
+            )
+        ]
+    )
